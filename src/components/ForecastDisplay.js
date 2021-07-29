08928ccdd810d72ca3e8c431387cad1469bd160a
joandy alejo garcia <63996685+Andy-Not@users.noreply.github.com>
2021-07-29 16:21:26 -0400

    working 7 day forecast

--- src/components/ForecastDisplay.js
+++ src/components/ForecastDisplay.js
@@ -3,38 +3,28 @@ import {useEffect, useState} from "react";
 
 const ForecastDisplay = (props) => {
 
- let tomorrowDay = new Date().getUTCDate();
+ const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
  const [forecastData, setForecastData] = useState(null);
- const [forecastDays, setForecastDays] = useState(null);
     console.log("FORECAST")
     useEffect(() => {
 
-        if (!forecastData && props.cityName){
-            let newUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=8d342f682d66b6e1370bc79bd312bcd2&units=imperial`;
-
-            let newUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${props.cityName}&appid=8d342f682d66b6e1370bc79bd312bcd2&units=imperial`;
-        axios.get(newUrl).then(response => {
-            console.log(newUrl)
-            console.log(response.data)
-            setForecastData(
-                {
-                    forecast: response.data.list
-                }
-            );
-            console.log("FORECAST CALL");
-        });
-        }
-        if (!forecastDays && forecastData){
-            console.log("set")
-            setForecastDays(forecastData.forecast.map(item =>  (<div>{item.main.temp}   date: {item.dt_txt}</div>)));
-        }
-    },[props.cityName, forecastData, forecastDays])
-    console.log(forecastDays)
-    // console.log(forecastData.forecast[0].dt_txt.substr(8,2))
+if (!forecastData && props.coords){let newUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${props.coords.lat}&lon=${props.coords.lon}&exclude=minutely&appid=8d342f682d66b6e1370bc79bd312bcd2&units=imperial`;
+    axios.get(newUrl).then(response => {
+        console.log(newUrl)
+        console.log(response.data)
+        setForecastData(
+            {
+                forecast: response.data.daily
+            }
+        );
+        console.log("FORECAST CALL");
+    });}
+ console.log("loop in forecast")
+    },[forecastData, props.coords])
 
     return( <div>
                 <ul>
-                    {forecastDays}
+                    {forecastData ? forecastData.forecast.map(item => (<li key={Math.random()}>Temp: {item.temp.day} day:{days[new Date(item.dt * 1000).getDay()]}</li>)): "LOADING"}
                 </ul>
             </div>)
 }
